cc561b4c9439fe968ba66bca025a7061d9efd95a
Usman Shaikh <shaikhusman57@gmail.com>
2024-11-04 15:52:06 +0530

    Rename error.middleware.ts to errorHandler.middleware.ts

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,5 @@
 import validate from './validate.middleware';
-import errorHandler from './error.middleware';
+import errorHandler from './errorHandler.middleware';
 import catchAsync from './catchAsync.middleware';
 
 export { validate, errorHandler, catchAsync };
