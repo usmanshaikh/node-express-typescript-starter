9aa715cdcfdfc60c4131ff344478571755b1035f
Usman Shaikh <shaikhusman57@gmail.com>
2024-11-06 20:26:23 +0530

    AppError statuscode and message rearrange

--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -2,7 +2,7 @@ export class AppError extends Error {
   public statusCode: number;
   public isOperational: boolean;
 
-  constructor(message: string, statusCode: number) {
+  constructor(statusCode: number, message: string) {
     super(message);
     this.statusCode = statusCode;
     this.isOperational = true; // Indicates if it's an operational error
