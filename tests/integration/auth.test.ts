ce1a6d7e0fa8bcf26c7b3a774f27c3f6556efaa1
Usman Shaikh <shaikhusman57@gmail.com>
2024-11-15 00:03:07 +0530

    Improve Auth route tests with detailed cases and token validation checks

--- tests/integration/auth.test.ts
+++ tests/integration/auth.test.ts
@@ -4,7 +4,9 @@ import { StatusCodes } from 'http-status-codes';
 import { MESSAGES } from '../../src/constants';
 import { User } from '../../src/models';
 import app from '../../src/app';
+import redisClient from '../../src/config/redisClient';
 
+// Helper function to generate dummy user data
 const generateUser = () => ({
   name: faker.person.fullName(),
   email: faker.internet.email().toLowerCase(),
@@ -12,11 +14,14 @@ const generateUser = () => ({
 });
 
 describe('Auth Routes', () => {
+  let refreshToken: string;
   const dummyUser = generateUser();
+
   describe('POST /auth/register', () => {
-    test(`should return ${StatusCodes.CREATED} and successfully register user if request data is ok`, async () => {
+    test('should register a user successfully and return a 201 status code', async () => {
       const res = await request(app).post('/auth/register').send(dummyUser);
       const { password, ...expectedUserData } = dummyUser;
+
       expect(res.statusCode).toBe(StatusCodes.CREATED);
       expect(res.body.data).toMatchObject({
         ...expectedUserData,
@@ -26,17 +31,22 @@ describe('Auth Routes', () => {
       });
     });
 
-    test(`should return ${StatusCodes.BAD_REQUEST} error if email is already used`, async () => {
+    test('should return a 400 status code if email is already used', async () => {
       const res = await request(app).post('/auth/register').send(dummyUser);
+
       expect(res.statusCode).toBe(StatusCodes.BAD_REQUEST);
       expect(res.body.message).toBe(MESSAGES.EMAIL_ALREADY_TAKEN);
     });
   });
+
   describe('POST /auth/login', () => {
     const { name, ...payload } = dummyUser;
-    test(`should return ${StatusCodes.OK} and successfully Login`, async () => {
+
+    test('should login a user successfully and return a 200 status code with tokens', async () => {
       const res = await request(app).post('/auth/login').send(payload);
       const { message, data } = res.body;
+      refreshToken = data.tokens.refresh.token;
+
       expect(res.statusCode).toBe(StatusCodes.OK);
       expect(message).toBe(MESSAGES.LOGIN_SUCCESS);
       expect(data).toMatchObject({
@@ -52,9 +62,36 @@ describe('Auth Routes', () => {
           refresh: { token: expect.any(String), expires: expect.any(String) },
         },
       });
+
+      // Delay to allow different 'iat' timestamps in tokens
+      await new Promise((resolve) => setTimeout(resolve, 2000));
     });
   });
+
+  describe('POST /auth/refresh-tokens', () => {
+    test('should refresh tokens successfully and return a 200 status code', async () => {
+      const res = await request(app)
+        .post('/auth/refresh-tokens')
+        .send({ refreshToken });
+      const { message } = res.body;
+      expect(res.statusCode).toBe(StatusCodes.OK);
+      expect(message).toBe(MESSAGES.TOKENS_REFRESHED_SUCCESS);
+    });
+
+    test('should return a 403 status code when trying to use a blacklisted refresh token', async () => {
+      const res = await request(app)
+        .post('/auth/refresh-tokens')
+        .send({ refreshToken });
+
+      console.log('Token state in Redis when blacklisted:', await redisClient.get(refreshToken));
+
+      const { message } = res.body;
+      expect(res.statusCode).toBe(StatusCodes.FORBIDDEN);
+      expect(message).toBe(MESSAGES.INVALID_REFRESH_TOKEN);
+    });
+  });
+
   afterAll(async () => {
-    await User.deleteMany();
+    await User.deleteMany(); // Clean up users after tests
   });
 });
