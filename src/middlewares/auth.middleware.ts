0af5533301fe9872a16f2d6a820e8745d4752e9d
Usman Shaikh <shaikhusman57@gmail.com>
2025-01-27 23:24:39 +0530

    Refactor authentication middleware to improve response handling for missing authorization headers and tokens

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -12,23 +12,26 @@ const authenticateJWT = (
   req: CustomRequest,
   res: Response,
   next: NextFunction,
-): void | Response => {
+): void => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({ message: MESSAGES.AUTH_HEADER_REQUIRED });
+    res.status(401).json({ message: MESSAGES.AUTH_HEADER_REQUIRED });
+    return;
   }
 
   const token = authHeader.split(' ')[1];
 
   if (!token) {
-    return res.status(401).json({ message: MESSAGES.AUTH_TOKEN_REQ });
+    res.status(401).json({ message: MESSAGES.AUTH_TOKEN_REQ });
+    return;
   }
 
   const payload = jwtHelper.verifyJwtToken(token);
 
   if (!payload || typeof payload.sub !== 'string') {
-    return res.status(403).json({ message: MESSAGES.INVALID_TOKEN });
+    res.status(403).json({ message: MESSAGES.INVALID_TOKEN });
+    return;
   }
 
   req.user = new Types.ObjectId(payload.sub);
